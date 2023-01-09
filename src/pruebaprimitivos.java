ce1ef6aaac56f053e84fe0d24d9c877571bc4520
SaraLi9789 <Sariiitaa9789@gmail.com>
2023-01-09 14:15:17 +0100

    Primeros commit

---
+++ src/pruebaprimitivos.java
@@ -0,0 +1,16 @@
+
+public class pruebaprimitivos {
+
+	public static void main(String[] args) {
+		int num = 2;
+		int contador = 1;
+		
+		while (contador <= 32 ) {
+			System.out.println("2^" + contador + "=" + num);
+			num = num *2; 
+			contador = contador+1;
+		} //while
+		
+	}//main
+
+}//class
